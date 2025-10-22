96d733bf41ac5269d553ffe126db7289ee5f360b
lacamillaa <12999356@itisrossi.vi.it>
2025-10-22 10:19:18 +0200

    implemented function to print struct variable

--- main.c
+++ main.c
@@ -7,10 +7,19 @@ struct Cocktail {
     short int alcoholPercent;
 };
 
+void stampa(struct Cocktail cocktail);
+
 int main(void) {
     struct Cocktail mojito;
     mojito.nome = "Mojito";
     mojito.price = 3.5f;
     mojito.alcoholPercent = 40;
+    stampa(mojito);
     return 0;
+}
+
+void stampa(struct Cocktail cocktail) {
+    printf("%s:\n", cocktail.nome);
+    printf("Price: %.2feur\t", cocktail.price);
+    printf("%d%% alcohol", cocktail.alcoholPercent);
 }
