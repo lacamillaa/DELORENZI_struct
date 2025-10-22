203992716c08f004616d260eb46ad8405748022d
lacamillaa <12999356@itisrossi.vi.it>
2025-10-22 10:23:06 +0200

    implemented pointer to variable of struct type

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     // NON è un oggetto
@@ -7,19 +8,21 @@ typedef struct {
     short int alcoholPercent;
 } Cocktail;
 
-void stampa(Cocktail cocktail);
+void stampa(Cocktail *cocktail);
 
 int main(void) {
-    Cocktail mojito;
-    mojito.nome = "Mojito";
-    mojito.price = 3.5f;
-    mojito.alcoholPercent = 40;
+    Cocktail *mojito = malloc(sizeof(Cocktail));
+    mojito->nome = "Mojito";
+    mojito->price = 3.5f;
+    mojito->alcoholPercent = 40;
     stampa(mojito);
+    free(mojito);
+    mojito = NULL;
     return 0;
 }
 
-void stampa(Cocktail cocktail) {
-    printf("%s:\n", cocktail.nome);
-    printf("Price: %.2feur\t", cocktail.price);
-    printf("%d%% alcohol", cocktail.alcoholPercent);
+void stampa(Cocktail *cocktail) {
+    printf("%s:\n", cocktail->nome);
+    printf("Price: %.2feur\t", cocktail->price);
+    printf("%d%% alcohol", cocktail->alcoholPercent);
 }
