2b0fbfdb1e7f7d50e701c4197c335469b04914a1
lacamillaa <12999356@itisrossi.vi.it>
2025-10-22 10:20:37 +0200

    used typedef to create struct reference keyword

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-struct Cocktail {
+typedef struct {
     // NON è un oggetto
     char *nome;
     float price;
     short int alcoholPercent;
-};
+} Cocktail;
 
-void stampa(struct Cocktail cocktail);
+void stampa(Cocktail cocktail);
 
 int main(void) {
-    struct Cocktail mojito;
+    Cocktail mojito;
     mojito.nome = "Mojito";
     mojito.price = 3.5f;
     mojito.alcoholPercent = 40;
@@ -18,7 +18,7 @@ int main(void) {
     return 0;
 }
 
-void stampa(struct Cocktail cocktail) {
+void stampa(Cocktail cocktail) {
     printf("%s:\n", cocktail.nome);
     printf("Price: %.2feur\t", cocktail.price);
     printf("%d%% alcohol", cocktail.alcoholPercent);
